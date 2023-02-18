ab02f03e3a901cd065ed9ed514162a06ac0dad1c
Leifsgard <Leifsgard@outlook.com>
2023-02-18 15:03:38 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rasmus Leifsgård on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
