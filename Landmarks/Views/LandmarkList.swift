341186fd35e79e43e596d8d48147c0b1d1f01ccc
Leifsgard <Leifsgard@outlook.com>
2023-02-19 16:53:17 +0100

    Updates

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Rasmus Leifsgård on 2023-02-19.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 14 Plus", "iPhone 14", "iPad (10th generation)", "iPhone XS"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }}
